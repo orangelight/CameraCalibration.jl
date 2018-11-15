eac88b8b52a9c73b661e5140c28aaf7b7490a2ae
Alex <alex.deneau17@gmail.com>
2018-11-14 23:49:48 -0500

    estimateHomography works

--- cameracali.jl
+++ cameracali.jl
@@ -5,29 +5,34 @@ estimateHomography - returns the estimated homography matrix H, such that b_j = 
 A and B are in the form 3xN (each row is a point)
 '''
 """
-function estimateHomography(a::Array{Real,2}, b::Array{Real,2})
+function estimateHomography(a::Array{<:Real,2}, b::Array{<:Real,2})
     if size(a) != size(b)
         error("src and dst must have same dimensions")
     end
     n = size(a)[2] #number of points
     #normalisation matrices
     nA = getNormalisationMatrix(a)
-    nB = getNormalisationMatrix(a)
+    nB = getNormalisationMatrix(b)
     m = zeros((2*n, 9))
 
     #construct matrix for solving
-    for j  = 1:(n-1)
+    for j  = 1:n
         k = 2*j
-        ap = nA*a[:,n]
-        ab = nB*b[:,n]
+        ap = nA*a[:,j]
+        bp = nB*b[:,j]
         # M_{k,*} = (x′_a, y′_a, 1, 0, 0, 0, −x′_ax′_b , −y′_ax′_b, −x′_b )
-        m[:,k-1] = [ap[1] ap[2] 1 0 0 0 -ap[1]*bp[1] -ap[2]*bp[1] -bp[1]]
+        m[k-1,:] = [ap[1] ap[2] 1 0 0 0 -ap[1]*bp[1] -ap[2]*bp[1] -bp[1]]
         # M_{k+1,*} = (0, 0, 0, x′_a , y′_a , 1, −x′_ay′_b, −y′_ay′_b , −y′_b )
-        m[:,k] = [0 0 0 ap[1] ap[2] 1 -ap[1]*bp[2] -ap[2]*bp[2] -bp[2]]
+        m[k,:] = [0 0 0 ap[1] ap[2] 1 -ap[1]*bp[2] -ap[2]*bp[2] -bp[2]]
+
     end
 
     #solve
-    u, d, vt = svd(m)
+    u, d, v = svd(m, full = true)
+    h = v[:,9] #last column of v
+	println(h)
+    hp = reshape(h, 3, 3)'
+    return inv(nB)*hp*nA
 end
 """
 getNormalisationMatrix - returns normalisation matrix of points x
@@ -35,7 +40,7 @@ getNormalisationMatrix - returns normalisation matrix of points x
 
 '''
 """
-function getNormalisationMatrix(x::Array{Real, 2})
+function getNormalisationMatrix(x::Array{<:Real, 2})
     m = mean(x[1:2, :], dims = 2)
     v = var(x[1:2, :], dims = 2, corrected = false, mean = m)
     #scale values (182)
@@ -45,3 +50,9 @@ function getNormalisationMatrix(x::Array{Real, 2})
     n = [sx 0 -sx*m[1]; 0 sy -sy*m[2]; 0 0 1]
     n
 end
+
+"""
+tests
+asample = [182 535 171 537; 350 358 553 563; 1 1 1 1]
+bsample = [0 888 0 888; 0 0 500 500; 1 1 1 1]
+"""
